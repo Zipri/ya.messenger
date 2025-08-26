5eee4c0f7c6fa5eeddaf2bb3c2e22de2f2c3d9e2
Seroshtan_KV <seroshtan_kv@rtsoft.ru>
2025-08-26 21:49:48 +0300

    [sprint_1] Создан общий компонент Input и использован в странице авторизации

---
+++ src/components/input/input.ts
@@ -0,0 +1,24 @@
+import { compile } from 'handlebars';
+import './input.scss';
+import inputTemplate from './input.hbs?raw';
+
+interface InputProps {
+    id: string;
+    name: string;
+    label: string;
+    type?: string;
+    value?: string;
+    placeholder?: string;
+    required?: boolean;
+}
+
+export class Input {
+    private template = compile(inputTemplate);
+
+    render(props: InputProps) {
+        return this.template({
+            type: 'text',
+            ...props,
+        });
+    }
+}
--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,9 +1,32 @@
 import './login.scss';
 import { compile } from 'handlebars';
 import loginTemplate from './login.hbs?raw';
+import { Input } from '../../components';
 
 export class LoginPage {
+    private template = compile(loginTemplate);
+    private input = new Input();
+
     render() {
-        return compile(loginTemplate)({});
+        const loginInput = this.input.render({
+            id: 'login',
+            name: 'login',
+            label: 'Логин',
+            type: 'text',
+            value: 'ivanivanov',
+        });
+
+        const passwordInput = this.input.render({
+            id: 'password',
+            name: 'password',
+            label: 'Пароль',
+            type: 'password',
+            value: '••••••••••',
+        });
+
+        return this.template({
+            loginInput,
+            passwordInput,
+        });
     }
 }

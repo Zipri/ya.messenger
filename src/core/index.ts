1da48ce6cbb9444226016d22df467bddc8e6c6ba
Seroshtan_KV <seroshtan_kv@rtsoft.ru>
2025-09-17 21:15:29 +0300

    [sprint_2_1] Перенос HTTPTransport

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,5 +1,4 @@
 import { EventBus } from './eventBus';
 import { Block } from './block';
-import { HTTPTransport } from './http';
 
-export { EventBus, Block, HTTPTransport };
+export { EventBus, Block };

f6edebbf334a2e9fcde75eb1a81113525d6e2662
Seroshtan_KV <seroshtan_kv@rtsoft.ru>
2025-10-07 14:50:35 +0300

    [sprint-4] доп прекомит

--- src/ui/pages/chat/dialog/userList/index.ts
+++ src/ui/pages/chat/dialog/userList/index.ts
@@ -1,2 +1,1 @@
 export { UserList } from './userList';
-
--- src/utils/mydash/last.ts
+++ src/utils/mydash/last.ts
@@ -4,9 +4,9 @@
  * @returns последний элемент массива или undefined, если массив невалидный или пустой
  */
 function last<T>(list: T[]): T | undefined {
-    if (!Array.isArray(list)) return undefined;
-    if (list.length === 0) return undefined;
-    return list[list.length - 1];
+  if (!Array.isArray(list)) return undefined;
+  if (list.length === 0) return undefined;
+  return list[list.length - 1];
 }
 
 export default last;
